3bb368d8fc773616fabbfce1c555289a00d46da4
cbaadani <cbaadani@gmail.com>
2019-12-22 17:40:19 +0200

    sleep execute

--- SleepCommand.cpp
+++ SleepCommand.cpp
@@ -3,7 +3,10 @@
 //
 
 #include "SleepCommand.h"
+#include <thread>
 
 int SleepCommand::execute(vector<string> params) {
-
+  int num = stoi(params[1]);
+  this_thread::sleep_for(chrono::milliseconds(num));
+  return 2;
 }
